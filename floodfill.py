8d03e2c533d0abce7b20d79a3a31eba70c31e1e4
BMC98 <44408428+BMC98@users.noreply.github.com>
2021-05-15 15:28:40 +0200

    Add files via upload

--- floodfill.py
+++ floodfill.py
@@ -3,20 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import data, filters, color, morphology, io
 from skimage.segmentation import flood, flood_fill
-
+from skimage.color import rgb2gray
 
 img = io.imread('Images/2.png')
 
+img_sobel = filters.sobel(img)
 
-flooded = flood_fill(img, (292, 171), 255, tolerance = 10)
-fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
+img_flood = flood(img_sobel, (240,265), tolerance = 0.05)
+
+floodfilled = flood_fill(img, (292, 171), 255, tolerance = 10)
+
+fig, ax = plt.subplots(ncols=4, figsize=(40, 40))
+
 
 ax[0].imshow(img, cmap = plt.cm.gray)
 ax[0].set_title('Original')
 ax[0].axis = 'off'
 
-ax[1].imshow(flooded, cmap = plt.cm.gray)
-ax[1].set_title('Flooded')
+
+ax[1].imshow(img_sobel)
+ax[1].set_title('Sobel')
 ax[1].axis = 'off'
 
+ax[2].imshow(img_flood)
+ax[2].set_title('Flooded')
+ax[2].axis = 'off'
+
+ax[3].imshow(floodfilled)
+ax[3].set_title('Floodfilled')
+ax[3].axis = 'off'
+
+fig.tight_layout()
 plt.show()
