7afb3741132f7d054fcc2e43300db0c843b00e36
BMC98 <44408428+BMC98@users.noreply.github.com>
2021-07-29 10:20:20 +0200

    Add files via upload

---
+++ unet_model.py
@@ -0,0 +1,76 @@
+from keras.models import Model 
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda, Activation
+from keras import regularizers 
+
+img_height = 512
+img_width = 512 
+img_channels = 3 
+classes = 5 
+
+def build_unet_model(classes, img_height, img_width, img_channels):
+	inputs = Input((img_width, img_height, img_channels))
+	i = Lambda(lambda x: x / 255.0)(inputs)
+
+	#Contraction
+	c1 = Conv2D(8, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(i)
+	c1 = Dropout(0.1)(c1)
+	c1 = Conv2D(8, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same', kernel_regularizer = 'l2')(c1)
+	p1 = MaxPooling2D((2, 2))(c1)
+
+	c2 = Conv2D(16, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(p1)
+	c2 = Dropout(0.1)(c2)
+	c2 = Conv2D(16, (3, 3),activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(c2)
+	p2 = MaxPooling2D((2, 2))(c2)
+
+	c3 = Conv2D(32, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(p2)
+	c3 = Dropout(0.1)(c3)
+	c3 = Conv2D(32, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(c3)
+	p3 = MaxPooling2D((2,2))(c3)
+
+	c4 = Conv2D(64, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(p3)
+	c4 = Dropout(0.1)(c4)
+	c4 = Conv2D(64, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(c4)
+	p4 = MaxPooling2D((2,2))(c4)
+
+	c5 = Conv2D(128, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(p4)
+	c5 = Dropout(0.1)(c5)
+	c5 = Conv2D(128, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(c5)
+
+	#Expansion
+	u6 = Conv2DTranspose(64, (2, 2), strides = (2, 2), padding = 'same', kernel_regularizer = 'l2')(c5)
+	u6 = concatenate([u6, c4])
+	c6 = Conv2D(64, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(u6)
+	c6 = Dropout(0.1)(c6)
+	c6 = Conv2D(64, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(c6)
+
+	u7 = Conv2DTranspose(32, (2, 2), strides = (2, 2), padding = 'same', kernel_regularizer = 'l2')(c6)
+	u7 = concatenate([u7, c3])
+	c7 = Conv2D(32, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(u7)
+	c7 = Dropout(0.1)(c7)
+	c7 = Conv2D(32, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(c7)
+
+	u8 = Conv2DTranspose(16, (2, 2), strides = (2, 2), padding = 'same', kernel_regularizer = 'l2')(c7)
+	u8 = concatenate([u8, c2])
+	c8 = Conv2D(16, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(u8)
+	c8 = Dropout(0.1)(c8)
+	c8 = Conv2D(16, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(c8)
+
+	u9 = Conv2DTranspose(8, (2, 2), strides = (2, 2), padding = 'same', kernel_regularizer = 'l2')(c8)
+	u9 = concatenate([u9, c1])
+	c9 = Conv2D(8, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(u9)
+	c9 = Dropout(0.1)(c9)
+	c9 = Conv2D(8, (3, 3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = 'l2')(c9)
+
+	output = Conv2D(classes, (1, 1), name="classificatie")(c9)
+	output = Activation('softmax', name="classificatie_act", dtype='float32')(output)
+
+	model = Model(inputs=[inputs], outputs=[output])
+
+	model.compile()
+	print(model.summary())
+
+	return model 
+
+
+
+build_unet_model(classes, img_height, img_width, img_channels)
---
+++ unet_run_peregrine.py
@@ -0,0 +1,227 @@
+from unet_model import build_unet_model 
+
+import tensorflow_addons as tfa
+from tensorflow.keras import utils
+#from keras.utils import to_categorical
+#from keras.metrics import MeanIoU
+from tensorflow.keras import metrics
+from tensorflow.keras import backend as K
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.utils import class_weight
+import os
+import glob
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+
+
+train_images = np.load(sys.argv[1])
+train_masks = np.load(sys.argv[2])
+
+img_height = 512
+img_width = 512 
+img_channels = 4 
+classes = 5 
+
+def get_model():
+    return build_unet_model(classes=classes, img_height=img_height, img_width=img_width, img_channels=img_channels)
+
+
+def DiceLoss(targets, inputs, smooth=1e-6):
+    
+    #flatten label and prediction tensors
+    inputs = K.flatten(inputs)
+    targets = K.flatten(targets)
+    
+    intersection = K.sum(K.dot(targets, inputs))
+    dice = (2*intersection + smooth) / (K.sum(targets) + K.sum(inputs) + smooth)
+    return 1 - dice
+
+#Encoder for the values of the labels
+
+def categorical_focal_loss(gamma=2.0, alpha=0.25):
+    """
+    Implementation of Focal Loss from the paper in multiclass classification
+    Formula:
+        loss = -alpha*((1-p)^gamma)*log(p)
+    Parameters:
+        alpha -- the same as wighting factor in balanced cross entropy
+        gamma -- focusing parameter for modulating factor (1-p)
+    Default value:
+        gamma -- 2.0 as mentioned in the paper
+        alpha -- 0.25 as mentioned in the paper
+    """
+    def focal_loss(y_true, y_pred):
+        # Define epsilon so that the backpropagation will not result in NaN
+        # for 0 divisor case
+        epsilon = K.epsilon()
+        # Add the epsilon to prediction value
+        #y_pred = y_pred + epsilon
+        # Clip the prediction value
+        y_pred = K.clip(y_pred, epsilon, 1.0-epsilon)
+        # Calculate cross entropy
+        cross_entropy = -y_true*K.log(y_pred)
+        # Calculate weight that consists of  modulating factor and weighting factor
+        weight = alpha * y_true * K.pow((1-y_pred), gamma)
+        # Calculate focal loss
+        loss = weight * cross_entropy
+        # Sum the losses in mini_batch
+        loss = K.sum(loss, axis=1)
+        return loss
+    
+    return focal_loss
+
+def jaccard_distance_loss(y_true, y_pred, smooth=100):
+    """
+    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
+            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+    
+    The jaccard distance loss is usefull for unbalanced datasets. This has been
+    shifted so it converges on 0 and is smoothed to avoid exploding or disapearing
+    gradient.
+    
+    Ref: https://en.wikipedia.org/wiki/Jaccard_index
+    
+    @url: https://gist.github.com/wassname/f1452b748efcbeb4cb9b1d059dce6f96
+    @author: wassname
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
+
+def dice_coef(y_true, y_pred, smooth=1):
+    """
+    Dice = (2*|X & Y|)/ (|X|+ |Y|)
+         =  2*sum(|A*B|)/(sum(A^2)+sum(B^2))
+    ref: https://arxiv.org/pdf/1606.04797v1.pdf
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
+
+def dice_coef_loss(y_true, y_pred):
+    return 1-dice_coef(y_true, y_pred)
+
+labelencoder = LabelEncoder()
+n, h, w = train_masks.shape
+train_masks_reshaped = train_masks.reshape(-1,1)
+train_masks_reshaped_encoded = labelencoder.fit_transform(train_masks_reshaped)
+train_masks_encoded_original_shape = train_masks_reshaped_encoded.reshape(n, h, w)
+
+np.unique(train_masks_encoded_original_shape)
+
+train_images = np.expand_dims(train_images, axis=3)
+train_images = utils.normalize(train_images, axis=1)
+
+train_masks_input = np.expand_dims(train_masks_encoded_original_shape, axis=3)
+
+from sklearn.model_selection import train_test_split
+X1, X_test, y1, y_test = train_test_split(train_images, train_masks_input, test_size = 0.15, random_state = 0)
+
+X_train, X_do_not_use, y_train, y_do_not_use = train_test_split(X1, y1, test_size = 0.2, random_state = 0)
+X_train = np.squeeze(X_train)
+X_test = np.squeeze(X_test)
+print("Class values in the dataset are ... ", np.unique(y_train))  # 0 is the background/few unlabeled 
+
+train_masks_cat = utils.to_categorical(y_train, num_classes=classes)
+y_train_cat = train_masks_cat.reshape((y_train.shape[0], y_train.shape[1], y_train.shape[2], classes))
+
+test_masks_cat = utils.to_categorical(y_test, num_classes=classes)
+y_test_cat = test_masks_cat.reshape((y_test.shape[0], y_test.shape[1], y_test.shape[2], classes))
+
+class_weights = class_weight.compute_class_weight('balanced',
+                                                 np.unique(train_masks_reshaped_encoded),
+                                                 train_masks_reshaped_encoded)
+
+class_weights = dict(enumerate(class_weights))
+print("Class weights are...:", class_weights)
+
+
+IMG_HEIGHT = X_train.shape[1]
+IMG_WIDTH  = X_train.shape[2]
+IMG_CHANNELS = X_train.shape[3] 
+
+cce = losses.SparseCategoricalCrossentropy(reduction=losses.Reduction.NONE)
+
+def weighted_ce(y_pred, y_target):
+    # y_pred and y_target should be of shape (batch_size, height, width, num_classes)
+
+    # Get the weights for each pixel
+    pp_w = y_target @ class_weights
+
+    # Get the cross entropy error for each pixel
+    pp_ce = cce(y_pred, y_target)
+
+    # Reweight each pixel and reduce
+    return K.mean(pp_ce * tf.squeeze(pp_w))
+
+model = get_model()
+print(X_train.shape)
+print(y_train_cat.shape)
+import tensorflow as tf
+model.compile(optimizer='adam', loss=weighted_ce, metrics=['acc'])
+
+history = model.fit(X_train, y_train_cat, 
+                    batch_size = 32, 
+                    verbose=1, 
+                    epochs=2, 
+                    validation_data=(X_test, y_test_cat), 
+                    #class_weight=class_weights,
+                    shuffle=False)
+
+_, acc = model.evaluate(X_test, y_test_cat)
+print("Accuracy is = ", (acc * 100.0), "%")
+
+model.save("groundtruth_model_hopefully_final.h5")
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('Loss.png')
+plt.show()
+plt.clf()
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+plt.plot(epochs, acc, 'y', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'r', label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig('Accuracy.png')
+plt.show()
+
+y_pred=model.predict(X_test)
+y_pred_argmax=np.argmax(y_pred, axis=3)
+
+IOU_keras = metrics.MeanIoU(num_classes = classes)  
+IOU_keras.update_state(y_test[:,:,:,0], y_pred_argmax)
+print("Mean IoU =", IOU_keras.result().numpy())
+
+values = np.array(IOU_keras.get_weights()).reshape(classes, classes)
+print(values)
+class1_IoU = values[0,0]/(values[0,0] + values[0,1] + values[0,2] + values[0,3] + values[0,4] + values[1,0] + values[2,0] + values[3,0] + values[4,0])
+class2_IoU = values[1,1]/(values[1,1] + values[1,0] + values[1,2] + values[1,3] + values[1,4] + values[0,1] + values[2,1] + values[3,1] + values[4,1])
+class3_IoU = values[2,2]/(values[2,2] + values[2,0] + values[2,1] + values[2,3] + values[2,4] + values[0,2] + values[1,2] + values[3,2] + values[4,2])
+class4_IoU = values[3,3]/(values[3,3] + values[3,0] + values[3,1] + values[3,2] + values[3,4] + values[0,3] + values[1,3] + values[2,3] + values[4,3])
+class5_IoU = values[4,4]/(values[4,4] + values[4,0] + values[4,1] + values[4,2] + values[4,3] + values[0,4] + values[1,4] + values[2,4] + values[3,4])
+
+print("IoU for class1 is: ", class1_IoU)
+print("IoU for class2 is: ", class2_IoU)
+print("IoU for class3 is: ", class3_IoU)
+print("IoU for class4 is: ", class4_IoU)
+print("IoU for class5 is: ", class5_IoU)
+
+plt.imshow(train_images[0, :,:,0], cmap='gray')
+plt.imshow(train_masks[0], cmap='gray')
+
